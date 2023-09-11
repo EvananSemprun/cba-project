1d2b91f43a536d677bdebb41462e8e018ec9d01e
EvananSemprun <evanansemprun@gmail.com>
2023-09-10 21:17:15 -0400

    Student

--- src/pages/Student/ThemeStudent.tsx
+++ src/pages/Student/ThemeStudent.tsx
@@ -1,13 +1,19 @@
 import Navbar from "../../components/Navbar";
-  function ThemeStudent() {
-    
-  
-    return (
-     <>
-     <Navbar/>
-     
-     </>
-    );
-  }
-  export default ThemeStudent;
-  
+import { Card, Badge, ScrollArea, Divider, Title, Tabs, Group } from '@mantine/core';
+function ThemeStudent() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Title align="center">
+        Tema: Nombre del tema
+      </Title>
+      <Card ml={15} mr={15} mt={45} h={755} shadow="sm" padding="lg" radius="lg" withBorder>
+        hola
+      </Card>
+
+    </>
+  );
+}
+export default ThemeStudent;
