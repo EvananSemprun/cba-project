3aed64397785de4331e4cfd3513e0683a61e99f1
Javier Diaz <javierdiazt406@gmail.com>
2023-09-13 03:52:21 -0400

    Card test

--- src/components/CardMaterial.tsx
+++ src/components/CardMaterial.tsx
@@ -1,94 +1,111 @@
 import { Card, Title, Progress, Text, Group, ActionIcon } from '@mantine/core';
 import { motion } from 'framer-motion';
-import { IconArrowRight } from '@tabler/icons-react';
+import { IconArrowRight, IconBook, IconHexagonalPyramidPlus, IconMath, IconPlant, IconTypography } from '@tabler/icons-react';
 
 const jsonData = [
-    {
-        "nombre": "Matematica",
-        "ruta": "#",
-        "Profesor": "Jose gonzalez",
-        "Progresoactividades": 50,
-        "Color": "#DCDDFC"
-    },
-    {
-        "nombre": "Fisica",
-        "ruta": "#",
-        "Profesor": "Jose gonzalez",
-        "Progresoactividades": 70,
-        "Color": "#F7CAB7"
-    },
-    {
-        "nombre": "Quimica",
-        "ruta": "#",
-        "Profesor": "Jose gonzalez",
-        "Progresoactividades": 20,
-        "Color": "#b7f7ca"
-    },
-    {
-        "nombre": "Historia",
-        "ruta": "#",
-        "Profesor": "Jose gonzalez",
-        "Progresoactividades": 10,
-        "Color": "#f7eab7"
-    }
+	{
+		"nombre": "Matematica",
+		"ruta": "#",
+		"Profesor": "Jose Gonzalez",
+		"Progresoactividades": 50,
+		"Color": "#DCDDFC",
+		"icon": {
+			"bg": "#D1D2F1",
+			"printIcon": <IconMath style={{ background: '#D1D2F1', padding: 12.5, borderRadius: '100%' }} />
+		}
+	},
+	{
+		"nombre": "Fisica",
+		"ruta": "#",
+		"Profesor": "Jose Gonzalez",
+		"Progresoactividades": 70,
+		"Color": "#F7CAB7",
+		"icon": {
+			"bg": "#D1D2F1",
+			"printIcon": <IconHexagonalPyramidPlus style={{ background: '#EEBFAB', padding: 12.5, borderRadius: '100%' }} />
+		}
+	},
+	{
+		"nombre": "Quimica",
+		"ruta": "#",
+		"Profesor": "Jose Gonzalez",
+		"Progresoactividades": 20,
+		"Color": "#EDF1FA",
+		"icon": {
+			"bg": "#D1D2F1",
+			"printIcon": <IconPlant style={{ background: '#E1E9F2', padding: 12.5, borderRadius: '100%' }} />
+		}
+	},
+	{
+		"nombre": "Historia",
+		"ruta": "#",
+		"Profesor": "Jose Gonzalez",
+		"Progresoactividades": 10,
+		"Color": "#f7eab7",
+		"icon": {
+			"bg": "#D1D2F1",
+			"printIcon": <IconBook style={{ background: '#F1DAA1', padding: 12.5, borderRadius: '100%' }} />
+		}
+	}
 ];
 
 function CardMaterial() {
-    return (
-        <>
-            <Group position="center">
-                {jsonData.map((item, index) => (
-                    <motion.div
-                    initial={{ opacity: 0, y: 50 }} // Configuración inicial de la animación
-                    animate={{ opacity: 1, y: 0 }}   // Configuración de la animación en su estado final
-                    transition={{ duration: 0.5 }}   // Duración de la animación en segundos
-                    whileHover={{ scale: 1.05 }} // Animación de hover
-                  >
-                      <a href="/MateriaStudent"  style={{ textDecoration: "none" }}>
-                        <Card
-                            key={index}
-                            w={300}
-                            h={270}
-                            mr={15}
-                            shadow="sm"
-                            radius="lg"
-                            style={{
-                                backgroundColor: item.Color,
-                                boxShadow: '0px 20px 20px rgba(0, 0, 0, 0.2)',
-                            }}
-                            withBorder
-                        >
-                            <Group position="apart" mt="md" mb="xs">
-                                <Title mb={15} order={3}>
-                                    {item.nombre}
-                                </Title>
-                            </Group>
-                            <Title mb={5} order={5}>
-                                Progreso
-                            </Title>
-                            <Progress
-                                label={item.Progresoactividades.toString()} // Convierte a cadena
-                                size="xl"
-                                radius="xl"
-                                mb={5}
-                                color="dark"
-                                value={item.Progresoactividades}
-                            />
-                            <Group position="apart" mt={95}>
-                                <Text fw={700}>{item.Profesor}</Text>
-                                    <ActionIcon variant="transparent">
-                                        <IconArrowRight size="3.125rem" />
-                                    </ActionIcon>
-                            </Group>
-                        </Card>
-                                </a>
+	return (
+		<>
+			<Group>
+				{jsonData.map((item, index) => (
+					<motion.div
+						initial={{ opacity: 0, y: 50 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{ duration: 0.5 }}
+						whileHover={{ scale: 1.05 }}
+					>
+						<a href="/MateriaStudent" style={{ textDecoration: "none" }}>
+							<Card
+								key={index}
+								w={300}
+								h={270}
+								mr={15}
+								radius="lg"
+								style={{
+									backgroundColor: item.Color,
+									boxShadow: '0px 6px 6px rgba(0, 0, 0, 0.1)',
+								}}
+								withBorder
+							>
+								{item.icon.printIcon}
+								<Group position="apart" mt="md" mb="xs">
+									<Title mb={15} order={3} fw={600}>
+										{item.nombre}
+									</Title>
+								</Group>
+								<Title mb={5} order={5} fw={450}>
+									Progreso
+								</Title>
+								<Progress
+									size="md"
+									radius="xl"
+									mb={5}
+									color="dark"
+									value={item.Progresoactividades}
+								/>
+								<Text ta='end' fw={600}>
+									{item.Progresoactividades}%
+								</Text>
+								<Group position="apart" mt={22.5}>
+									<Text fw={700}>{item.Profesor}</Text>
+									<ActionIcon variant="transparent">
+										<IconArrowRight size="3.125rem" />
+									</ActionIcon>
+								</Group>
+							</Card>
+						</a>
 
-                    </motion.div>
-                ))}
-
-            </Group>
-        </>
-    );
+					</motion.div>
+				))}
+			</Group>
+		</>
+	);
 }
 
 export default CardMaterial;
--- src/pages/Student/HomeStudent.tsx
+++ src/pages/Student/HomeStudent.tsx
@@ -1,14 +1,20 @@
 
-import { Card, Title, } from '@mantine/core';
+import { Card, Group, Title, Text } from '@mantine/core';
 import Cardm from '../../components/CardMaterial'
 import { motion } from 'framer-motion';
+import { IconArrowRight } from '@tabler/icons-react';
 function HomeStudent() {
   return (
     <>
       <Card w="100%" h="100%" style={{ borderRadius: '15px 0 0 15px' }}>
-        <Title mb={15} align="center" order={1}>
-          Materias cursadas
-        </Title>
+        <Group position='apart'>
+          <Title mb={15} order={2} fw={500}>
+            Mis Cursos
+          </Title>
+          <Title mb={15} order={4} fw={500}>
+            Ver todos <IconArrowRight style={{ paddingTop: 15 }}/>
+          </Title>
+        </Group>
        <Cardm/>
       </Card>
     </>
