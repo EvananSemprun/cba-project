b59942cb81be9f43de9ca1513f7be66039d17db9
EvananSemprun <evanansemprun@gmail.com>
2023-09-15 01:25:00 -0400

    student

--- src/pages/Student/Homeworkstudent.tsx
+++ src/pages/Student/Homeworkstudent.tsx
@@ -1,7 +1,15 @@
-import { Card, Title, Divider, createStyles, Group, Button } from '@mantine/core';
+import { Card, Title, Divider, createStyles, Group, Text, Button, Modal, useMantineTheme } from '@mantine/core';
 import Homeworkstudentcontent from '../../components/Student/Homeworkstudentcontent'
 
-function Homeworkstudent() {
+import { IconUpload, IconPhoto, IconX } from '@tabler/icons-react';
+import { Dropzone, DropzoneProps } from '@mantine/dropzone';
+import { useDisclosure } from '@mantine/hooks';
+
+function Homeworkstudent(props: Partial<DropzoneProps>) {
+
+    const [openedcoment, { open, close }] = useDisclosure(false);
+    const [openedUpload, { open: openu, close: closeu }] = useDisclosure(false);
+
 
     const useStyles = createStyles((theme) => ({
         containerCard: {
@@ -29,16 +37,80 @@ function Homeworkstudent() {
         fontFamily: 'Montserrat',
         borderRadius: '15px 0 0 15px'
     };
-
+    const theme = useMantineTheme();
 
     return (
         <>
+
+            <Modal opened={openedcoment} onClose={close} withCloseButton={false}>
+                <Title align="center">
+                    Comentarios del profesor
+                </Title>
+
+                <Text mt={15} align='justify'>
+
+                    Según tu desempeño en la tarea, te doy una puntuación de 18 sobre 20. ¡Excelente trabajo!
+
+                </Text>
+            </Modal>
+
+            <Modal opened={openedUpload} size="xl" onClose={closeu} withCloseButton={false}>
+                <Title align="center">
+                    Subir tarea
+                </Title>
+                <Dropzone
+                    h={250}
+                    p={50}
+                    onDrop={(files) => console.log('accepted files', files)}
+                    onReject={(files) => console.log('rejected files', files)}
+                    maxSize={3 * 1024 ** 2}
+                    accept={['image/png', 'image/jpeg', 'image/sgv+xml', 'image/gif', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/pdf', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel']}
+                    {...props}
+                >
+                    <Group position="center" spacing="xl" style={{ pointerEvents: 'none' }}>
+                        <Dropzone.Accept>
+                            <IconUpload
+                                size="3.2rem"
+                                stroke={1.5}
+                                color={theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 4 : 6]}
+                            />
+                        </Dropzone.Accept>
+                        <Dropzone.Reject>
+                            <IconX
+                                size="3.2rem"
+
+                                stroke={1.5}
+                                color={theme.colors.red[theme.colorScheme === 'dark' ? 4 : 6]}
+                            />
+                        </Dropzone.Reject>
+                        <Dropzone.Idle>
+                            <IconPhoto size="3.2rem" stroke={1.5} />
+                        </Dropzone.Idle>
+
+                        <div>
+                            <Text size="xl" inline>
+                                Arrastre los archivos aquí o haga clic para seleccionar los archivos
+                            </Text>
+                            <Text size="sm" color="dimmed" inline mt={7}>
+                                Adjunte tantos archivos como desee, cada archivo no debe superar los 10mb
+                            </Text>
+                        </div>
+                    </Group>
+                </Dropzone>
+                <Group position="center">
+
+                    <Button size="md" mt={25} color="green" radius="md">
+                        Subir
+                    </Button>
+                </Group>
+            </Modal>
+
             <Card style={titleStyle} className={classes.containerCard} w="100%" h="100%">
 
                 <Card radius="xl" withBorder  >
 
                     <Title align="center">
-                        Tema: Nombre del tema
+                        Tarea: Nombre del tarea
                     </Title>
 
                     <Divider my="sm" variant="dashed" />
@@ -53,11 +125,11 @@ function Homeworkstudent() {
 
                         <Group position="center">
 
-                            <Button size="md" mt={25} color="indigo" radius="xl">
+                            <Button onClick={openu} size="md" mt={25} color="indigo" radius="md">
                                 Subir tarea
                             </Button>
 
-                            <Button size="md" mt={25} color="indigo" radius="xl">
+                            <Button size="md" mt={25} color="indigo" radius="md">
                                 Descargar contenido
                             </Button>
 
@@ -65,8 +137,11 @@ function Homeworkstudent() {
 
                     </Card>
 
-                    <Group position="apart">
-                       
+                    <Group mt={24.5} position="apart">
+                        <Title order={1}>Nota 18/20 pts</Title>
+                        <Button onClick={open} size="md" color="indigo" radius="md">
+                            Comentarios del profesor
+                        </Button>
                     </Group>
                 </Card>
             </Card>
